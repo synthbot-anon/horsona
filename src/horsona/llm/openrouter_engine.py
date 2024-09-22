9bce01fcf601ff12b8c9b14259d2acc0293f7eea
Synthbot Anon <synthbot.anon@gmail.com>
2024-09-22 01:08:10 -0700

    add openrouter engine (untested)

---
+++ src/horsona/llm/openrouter_engine.py
@@ -0,0 +1,31 @@
+import os
+
+from openai import AsyncOpenAI
+
+from .oai_engine import AsyncOAIEngine
+
+
+class AsyncOpenAIEngine(AsyncOAIEngine):
+    """
+    An asynchronous implementation of ChatEngine for interacting with Cerebras models.
+
+    This class provides an asynchronous interface for querying Cerebras language models.
+
+    Attributes:
+        model (str): The name of the Cerebras model to use.
+        client (AsyncCerebras): An instance of the asynchronous Cerebras client for API interactions.
+
+    Inherits from:
+        AsyncChatEngine
+    """
+
+    def __init__(self, model: str, *args, url: str, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.client = AsyncOpenAI(
+            base_url=url,
+            api_key=os.environ.get("OPENROUTER_API_KEY"),
+        )
+
+    async def create(self, **kwargs):
+        return await self.client.chat.completions.create(model=self.model, **kwargs)
