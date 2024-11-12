31ec96480caaaf21aad3e18ccb4ae84f26702501
Synthbot Anon <synthbot.anon@gmail.com>
2024-11-12 03:22:04 -0800

    update format.sh to check only committed files

--- format.sh
+++ format.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+if printf '%s\n' "$@" | grep -q '[[:space:]]'; then
+    echo "Error: Filenames cannot contain whitespace" >&2
+    exit 1
+fi
+
 projroot=$(dirname "$(readlink -f $0)")
+echo $projroot > ~/123
 cd "$projroot"
 
-poetry run ruff check --select I --fix
-poetry run ruff format
+pyfiles=$(printf '%s\n' "$@" | grep '\.py$')
+echo "pyfiles:" $pyfiles "a"
+
+if [ -z "$pyfiles" ] && [ $# -ne 0 ]; then
+    exit 0
+fi
+
+poetry run ruff check --select I --fix $pyfiles
+poetry run ruff format --respect-gitignore $pyfiles
 
